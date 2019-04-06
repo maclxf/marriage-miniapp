5546e79ff49d735aa3c913b26f4b11adb8d2fa64
Jiezhi <Jiezhi.G@gmail.com>
2019-04-06 17:20:54 +0800

    format code

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,165 +1,173 @@
 // pages/invitation/index.js
 const app = getApp()
-var server = app.globalData.server;
+var server = app.globalData.server + "/info";
 var appid = app.globalData.appid;
+const uid = app.globalData.uid;
 var touchDot = 0; //触摸时的原点  
 var time = 0; // 时间记录，用于滑动时且时间小于1s则执行左右滑动 
 var interval = ""; // 记录/清理时间记录 
 Page({
 
-    /**
-     * 页面的初始数据
-     */
-    data: {
-        animationData: "",
-        userInfo: {},
-        music_url: '',
-        isPlayingMusic: true
-    },
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    animationData: "",
+    userInfo: {},
+    music_url: '',
+    isPlayingMusic: true
+  },
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function(options) {
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
 
-        //创建动画
-        var animation = wx.createAnimation({
+    //创建动画
+    var animation = wx.createAnimation({
 
-            duration: 3600,
-            timingFunction: "ease",
-            delay: 600,
-            transformOrigin: "50% 50%",
+      duration: 3600,
+      timingFunction: "ease",
+      delay: 600,
+      transformOrigin: "50% 50%",
 
+    })
+
+
+    animation.scale(0.9).translate(10, 10).step(); //边旋转边放大
+
+
+    //导出动画数据传递给组件的animation属性。
+    this.setData({
+      animationData: animation.export(),
+    })
+
+    var that = this
+    wx.showLoading({ //期间为了显示效果可以添加一个过度的弹出框提示“加载中”  
+      title: '加载中',
+      icon: 'loading',
+    });
+    wx.request({
+      url: server,
+      method: 'GET',
+      data: {
+        'uid': uid,
+        'appid': appid
+      },
+      header: {
+        'Accept': 'application/json'
+      },
+      success: function(res) {
+        // console.log(res.data)
+        wx.hideLoading();
+        wx.playBackgroundAudio({
+          dataUrl: res.data.music,
+          title: '',
+          coverImgUrl: ''
+        })
+        wx.setStorage({
+          key: 'main',
+          data: res.data,
         })
 
-
-        animation.scale(0.9).translate(10, 10).step(); //边旋转边放大
-
-
-        //导出动画数据传递给组件的animation属性。
-        this.setData({
-            animationData: animation.export(),
-        })
-
-        var that = this
-        wx.showLoading({ //期间为了显示效果可以添加一个过度的弹出框提示“加载中”  
-            title: '加载中',
-            icon: 'loading',
+        that.setData({
+          mainInfo: res.data,
+          music_url: res.data.music
         });
-        wx.request({
-            url: server + "/info?uid=1",
-            method: 'GET',
-            header: {
-                'Accept': 'application/json'
-            },
-            success: function(res) {
-                // console.log(res.data)
-                wx.hideLoading();
-                // wx.playBackgroundAudio({
-                //     dataUrl: res.data.music_url,
-                //     title: '',
-                //     coverImgUrl: ''
-                // })
+      }
+    })
+  },
 
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
 
-                that.setData({
-                    mainInfo: res.data,
-                    music_url: res.data.music
-                });
-            }
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+    var that = this;
+    //console.log(that.data);
+    return {
+      title: that.data.mainInfo.share,
+      imageUrl: that.data.mainInfo.thumb,
+      path: 'pages/index/index',
+      success: function(res) {
+        wx.showToast({
+          title: '分享成功',
         })
-    },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function() {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function() {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function() {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function() {
-        var that = this;
-        //console.log(that.data);
-        return {
-            title: that.data.mainInfo.share,
-            imageUrl: that.data.mainInfo.thumb,
-            path: 'pages/index/index',
-            success: function(res) {
-                wx.showToast({
-                    title: '分享成功',
-                })
-            },
-            fail: function(res) {
-                // 转发失败
-                wx.showToast({
-                    title: '分享取消',
-                })
-            }
-        }
-    },
-    callhe: function(event) {
-        wx.makePhoneCall({
-            phoneNumber: this.data.mainInfo.he_tel
+      },
+      fail: function(res) {
+        // 转发失败
+        wx.showToast({
+          title: '分享取消',
         })
-    },
-    callshe: function(event) {
-        wx.makePhoneCall({
-            phoneNumber: this.data.mainInfo.she_tel
-        })
-    },
-    play: function(event) {
-        if (this.data.isPlayingMusic) {
-            wx.pauseBackgroundAudio();
-            this.setData({
-                isPlayingMusic: false
-            })
-        } else {
-            wx.playBackgroundAudio({
-                dataUrl: this.data.music_url,
-                title: '',
-                coverImgUrl: ''
-            })
-            this.setData({
-                isPlayingMusic: true
-            })
-        }
-    },
+      }
+    }
+  },
+  callhe: function(event) {
+    wx.makePhoneCall({
+      phoneNumber: this.data.mainInfo.he_tel
+    })
+  },
+  callshe: function(event) {
+    wx.makePhoneCall({
+      phoneNumber: this.data.mainInfo.she_tel
+    })
+  },
+  play: function(event) {
+    if (this.data.isPlayingMusic) {
+      wx.pauseBackgroundAudio();
+      this.setData({
+        isPlayingMusic: false
+      })
+    } else {
+      wx.playBackgroundAudio({
+        dataUrl: this.data.music_url,
+        title: '',
+        coverImgUrl: ''
+      })
+      this.setData({
+        isPlayingMusic: true
+      })
+    }
+  },
 })
